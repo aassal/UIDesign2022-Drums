17182c3f7005e75e3d2ce193885e893e7da0b994
aassal <assal.amina.h@gmail.com>
2022-04-24 01:49:44 -0400

    changed homepage layout slightly, changed secondary color, and chagned prev and next buttons to arrows

--- static/quiz.js
+++ static/quiz.js
@@ -12,6 +12,7 @@ function display_quiz(){
     var row = $("<div class='row'>");
     var col1 = $("<div class='col-md-6 contentdiv'>");
     var col2 = $("<div class='col-md-6 contentdiv'>");
+    var btngroup = $("<div class='center btn-group'></div>");
 
  
     title.appendTo($("#welcome_view"));
@@ -37,7 +38,7 @@ function display_quiz(){
         questiondiv.appendTo(quizform);
     });
     
-    var submitbutton = $('<input id="submitbtn" form="quiz" class="center btn btn-outline-warning" type="submit" value="Submit Choice">')
+    var submitbutton = $('<input id="submitbtn" form="quiz" class="center gold btn " type="submit" value="Submit Choice">')
     if (quiz["prevans"]!="-1"){
         $(submitbutton).attr("disabled", true)
         display_answer({"submitted": quiz["prevans"], "correct": quiz["rightchoice"]})
@@ -48,15 +49,19 @@ function display_quiz(){
     $(row).appendTo($("#welcome_view"));
 
     if (parseInt(quiz["id"])>1){
-        var prevlearn = $("<a href='/quiz/"+(parseInt(quiz["id"])-1).toString()+"' class='center btn btn-outline-warning'>Previous Question!</a>");
-        $(prevlearn).appendTo($("#final"));
+        var prevlearn = $("<a href='/quiz/"+(parseInt(quiz["id"])-1).toString()+"' class='btn gold'><svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='greenyellow' class='bi bi-arrow-left-square-fill' viewBox='0 0 16 16'> <path d='M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z'/></svg></a>");
+        $(prevlearn).appendTo($(btngroup));
+
+        $(btngroup).appendTo($("#final"));
     }
     submitbutton.appendTo($("#final"));
     if (parseInt(quiz["id"])<5){
-        var nextlearn = $("<a href='/quiz/"+(parseInt(quiz["id"])+1).toString()+"' class='center btn btn-outline-warning'>Next Question!</a>");
-        $(nextlearn).appendTo($("#final"));
+        var nextlearn = $("<a href='/quiz/"+(parseInt(quiz["id"])+1).toString()+"' class='btn gold'><svg xmlns='http://www.w3.org/2000/svg' width='25' height='25' fill='greenyellow' class='bi bi-arrow-right-square-fill' viewBox='0 0 16 16'> <path d='M0 14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v12zm4.5-6.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5a.5.5 0 0 1 0-1z'/></svg></a>");
+        $(nextlearn).appendTo($(btngroup));
+        $(btngroup).appendTo($("#final"));
     } else {
         var nextlearn = $("<a href='/quiz/end' class='center btn btn-outline-warning'>Submit Quiz!</a>");
+        
         $(nextlearn).appendTo($("#final"));
     }
 }
